7c48c7064a130590e3fb4d8c7879795d882f8c43
A-I-A <box112015@gmail.com>
2020-12-07 10:20:57 +0300

    Updates

---
+++ precache-manifest.f9713c2c5b82f5e132476c9b5f296702.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8de4ff6029ff50d1a7c73d7eb3a646e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b062cd282255fcb876d2",
+    "url": "/static/css/main.de24592c.chunk.css"
+  },
+  {
+    "revision": "e55a6e6b2e4493f12d5a",
+    "url": "/static/js/2.98bcaec2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.98bcaec2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b062cd282255fcb876d2",
+    "url": "/static/js/main.f8088ddd.chunk.js"
+  },
+  {
+    "revision": "ee13ce99935382ac6a78",
+    "url": "/static/js/runtime-main.90b03acc.js"
+  }
+]);
